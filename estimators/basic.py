5500d37c05582af74978a76a98cf80dc823a6dab
Chris Barrick <cbarrick1@gmail.com>
2017-12-01 16:57:04 -0500

    Update base estimator

--- estimators/basic.py
+++ estimators/basic.py
@@ -1,4 +1,6 @@
+import logging
 import sys
+from pathlib import Path
 
 import torch
 import torch.autograd as A
@@ -6,19 +8,23 @@ import torch.nn.functional as F
 import torch.utils.data as D
 
 
+logger = logging.getLogger(__name__)
+
+
 class Estimator:
     '''Wraps a torch network, optimizer, and loss to an sklearn-like estimator.
     '''
 
-    def __init__(self, net, opt, loss, cuda=None, dry_run=False):
-        '''Create a basic estimator.
+    def __init__(self, net, opt, loss, name='model', cuda=None, dry_run=False):
+        '''Create basic estimator.
 
         Args:
             net: The network to train.
             opt: The optimizer to step during training.
             loss: The loss function to minimize.
-            cuda: The cuda device. `None` means automatic. `False` means disabled.
-            dry_run: Cut loops short, for testing.
+            name: A name for the model.
+            cuda: The cuda device to use.
+            dry_run: Cut loops short.
         '''
         if cuda is None and torch.cuda.is_available():
             cuda = 0
@@ -31,6 +37,11 @@ class Estimator:
         self.loss = loss
         self.cuda = cuda
         self.dry_run = dry_run
+        self.reset()
+
+        self.path = Path(f'./_parameters/{self.name}.torch')
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        self.path.touch()
 
     def reset(self):
         '''Reset the estimator to it's initial state.
@@ -38,19 +49,49 @@ class Estimator:
         self.net.reset()
         return self
 
+    def save(self, path=None):
+        '''Saves the model parameters to disk.
+        '''
+        if path is None:
+            path = self.path
+        logger.info(f'saving {self.name} to {path}')
+        state = self.net.state_dict()
+        torch.save(state, str(path))
+        return self
+
+    def load(self, path=None):
+        '''Loads the model parameters from disk.
+        '''
+        if path is None:
+            path = self.path
+        logger.info(f'restoring {self.name} from {path}')
+        state = torch.load(str(path))
+        self.net.load_state_dict(state)
+        return self
+
     def variable(self, x, **kwargs):
-        '''Cast a tensor to a variable.
+        '''Cast a tensor to a `Variable` on the same cuda device as the network.
+
+        If the input is already a `Variable`, it is not wrapped.
 
         Args:
             x: The tensor to wrap.
             **kwargs: Passed to the `Variable` constructor.
-
-        Returns:
-            Returns a torch `Variable` on the same cuda device as the network.
-            If x is already a `Variable`, it is not wrapped, but it is moved to the GPU.
         '''
         if not isinstance(x, A.Variable):
             x = A.Variable(x, **kwargs)
+        if self.cuda is not False:
+            x = x.cuda(self.cuda, async=True)
+        return x
+
+    def tensor(self, x):
+        '''Cast some `x` to a torch `Tensor` on the same cuda device as the network.
+
+        Args:
+            x: The input to cast
+        '''
+        if isinstance(x, A.Variable):
+            x = x.detach().data
         if self.cuda is not False:
             x = x.cuda(self.cuda, async=True)
         return x
@@ -59,7 +100,7 @@ class Estimator:
         '''Get the list of trainable paramaters.
 
         Returns:
-            Returns a list of all parameters known to the optimizer.
+            A list of all parameters in the optimizer's `param_groups`.
         '''
         return [p for group in self.opt.param_groups for p in group['params']]
 
@@ -71,25 +112,25 @@ class Estimator:
             y: The class labels.
 
         Returns:
-            Returns the losses for this batch.
+            Returns the average loss for this batch.
         '''
-        self.net.train()
-        self.opt.zero_grad()
+        self.net.train()  # put the net in train mode, effects dropout layers etc.
+        self.opt.zero_grad()  # reset the gradients of the trainable variables.
         x = self.variable(x)
         y = self.variable(y)
         h = self.net(x)
         j = self.loss(h, y)
         j.backward()
         self.opt.step()
-        return j.data
+        return j.data.mean()
 
-    def fit(self, data, validation, max_epochs=100, patience=5, **kwargs):
+    def fit(self, train, validation=None, epochs=100, patience=50, **kwargs):
         '''Fit the model to a dataset.
 
         Args:
-            data: A dataset to fit.
+            train: A dataset to fit.
             validation: A dataset to use as the validation set.
-            max_epochs: The maximum number of epochs to spend training.
+            epochs: The maximum number of epochs to spend training.
             patience: Stop if the validation loss does not improve after this many epochs.
             **kwargs: Forwarded to torch's `DataLoader` class, with more sensible defaults.
 
@@ -98,39 +139,48 @@ class Estimator:
             Returns train loss if no validation set is given.
         '''
         kwargs.setdefault('shuffle', True)
-        kwargs.setdefault('pin_memory', self.cuda is not None)
-        data = D.DataLoader(data, **kwargs)
+        kwargs.setdefault('pin_memory', self.cuda is not False)
+        train = D.DataLoader(train, **kwargs)
 
         best_loss = float('inf')
         p = patience
-        for epoch in range(max_epochs):
+        for epoch in range(epochs):
 
             # Train
+            n = len(train)
             train_loss = 0
             print(f'epoch {epoch+1} [0%]', end='\r', flush=True, file=sys.stderr)
-            for i, (x, y) in enumerate(data):
+            for i, (x, y) in enumerate(train):
                 j = self.partial_fit(x, y)
-                train_loss += j.sum() / len(data.dataset)
-                progress = (i+1) / len(data)
+                train_loss += j / n
+                progress = (i+1) / n
                 print(f'epoch {epoch+1} [{progress:.2%}]', end='\r', flush=True, file=sys.stderr)
                 if self.dry_run:
                     break
-            print(f'epoch {epoch+1} [Train loss: {train_loss:8.6f}]', end='')
+            print(f'epoch {epoch+1} [Train loss: {train_loss:8.6f}]', end='', flush=True)
 
             # Validate
-            val_loss = self.test(validation)
-            print(f' [Validation loss: {val_loss:8.6f}]', flush=True)
+            if validation:
+                val_loss = self.test(validation, **kwargs)
+                print(f' [Validation loss: {val_loss:8.6f}]', end='')
+            print(flush=True)
 
-            # Convergence test
-            if val_loss < best_loss:
-                best_loss = val_loss
-                p = patience
+            # Early stopping
+            if validation:
+                loss = val_loss
+                if loss < best_loss:
+                    best_loss = loss
+                    p = patience
+                    self.save()
+                else:
+                    p -= 1
+                    if p == 0:
+                        self.load()
+                        break
             else:
-                p -= 1
-                if p == 0:
-                    break
+                loss = train_loss
 
-        return val_loss
+        return loss
 
     def predict(self, x):
         '''Apply the network to some input batch.
@@ -141,8 +191,8 @@ class Estimator:
         Returns:
             Returns the output of the network.
         '''
-        self.net.eval()
-        x = self.variable(x, volatile=True)
+        self.net.eval()  # put the net in eval mode, effects dropout layers etc.
+        x = self.variable(x, volatile=True)  # use volatile input to save memory when not training.
         h = self.net(x)
         return h.data
 
@@ -160,16 +210,22 @@ class Estimator:
         self.net.eval()
 
         if criteria is None:
+            # Default - the loss has a different signature than a metric
             x = self.variable(x, volatile=True)
             y = self.variable(y, volatile=True)
             h = self.net(x)
             j = self.loss(h, y)
             return j.data.mean()
 
-        h = self.predict(x)
         try:
+            # Fast path - try to use the GPU
+            h = self.predict(x)
+            y = self.tensor(y)
             return criteria(y, h)
+
         except (ValueError, TypeError):
+            # Slow path - most sklearn metrics cannot handle Tensors
+            logger.debug('computing a slow metric')
             h = h.cpu().numpy()
             y = y.cpu().numpy()
             return criteria(y, h)
@@ -186,8 +242,8 @@ class Estimator:
             Returns the result of `criteria(true, predicted)` averaged over all batches.
             The last incomplete batch is dropped by default.
         '''
-        kwargs.setdefault('drop_last', True)
-        kwargs.setdefault('pin_memory', self.cuda is not None)
+        kwargs.setdefault('pin_memory', self.cuda is not False)
+        kwargs['drop_last'] = True
         data = D.DataLoader(data, **kwargs)
         n = len(data)
         loss = 0
