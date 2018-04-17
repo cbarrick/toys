4fdf50b9b10e79c5cf630bf93537f0f1ac36be8c
Chris Barrick <cbarrick1@gmail.com>
2018-04-16 20:13:56 -0400

    Remove datasets (for now)

--- toys/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from torch.utils.data import *
-
-from . import pathology
-from . import utils
-
-from .mnist import MNIST
-from .mnist import FashionMNIST
--- toys/datasets/pathology/__init__.py
+++
@@ -1,3 +0,0 @@
-from .nuclei import NucleiSegmentation
-from .epi import EpitheliumSegmentation
-from .tubule import TubuleSegmentation
