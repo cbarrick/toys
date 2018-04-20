2b6be2333a73b1ff9c374f002a67d6a627349019
Chris Barrick <cbarrick1@gmail.com>
2018-04-20 16:26:33 -0400

    Add Subset, tweak base Dataset

--- toys/datasets/utils/utils.py
+++ toys/datasets/utils/utils.py
@@ -1,33 +1,55 @@
-from abc import ABC, abstractmethod
-
-import torch
-from torch.utils.data import Dataset as TorchDataset
+from abc import abstractmethod
+from typing import Sequence
 
 
-class Dataset(ABC, TorchDataset):
-    '''A wrapper around `torch.utils.data.Dataset`
-    with proper abstract method declarations.
+# The Protocol class introduced in Python 3.7 is an improvement over the older
+# ABC class. TODO: Consider dropping support for versions older than 3.7.
+# See PEP 544 <https://www.python.org/dev/peps/pep-0544/>
+try:
+    from typing import Protocol
+else:
+    from abc import ABC as Protocol
+
+
+class Dataset(Protocol):
+    '''An improved Dataset base class.
     '''
-    def __init__(self):
-        # ABC and TorchDataset are not cooperative with
-        # multiple inheritance, so we initialize them manually.
-        ABC.__init__(self)
-        Dataset.__init__(self)
-
     @abstractmethod
     def __getitem__(self, index):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def __len__(self):
-        pass
+        raise NotImplementedError
+
+
+class Subset(Dataset):
+    '''A non-empty subset of some other dataset.
+
+    Attributes:
+        dataset (Dataset):
+            The source dataset.
+        indices (Sequence[int]):
+            The indices of elements contained in this subset.
+    '''
+    def __init__(self, dataset, indices):
+        assert 0 < max(indices) < len(dataset)
+        assert 0 < min(indices) < len(dataset)
+        self.dataset = dataset
+        self.indices = indices
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, i):
+        return self.dataset[self.indices[i]]
 
 
 class Zip(Dataset):
     '''Zip many datasets into one, like `builtins.zip`.
     '''
     def __init__(self, *datasets):
-        assert len(datasets) > 0
+        assert 0 < len(datasets)
         for d in datasets: assert len(d) == len(datasets[0])
         self.datasets = datasets
 
