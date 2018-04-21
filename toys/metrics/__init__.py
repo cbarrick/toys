2feb5bf8b61ba5ec2b5a32b9ba07754ee728b054
Chris Barrick <cbarrick1@gmail.com>
2018-04-21 15:10:10 -0400

    s/toys.metrics.classification/toys.metrics.supervised/g

--- toys/metrics/__init__.py
+++ toys/metrics/__init__.py
@@ -2,11 +2,11 @@ from .core import Accumulator
 from .core import Mean
 from .core import Sum
 
-from .classification import Accuracy
-from .classification import TruePositives
-from .classification import FalsePositives
-from .classification import TrueNegatives
-from .classification import FalseNegatives
-from .classification import Precision
-from .classification import Recall
-from .classification import FScore
+from .supervised import Accuracy
+from .supervised import TruePositives
+from .supervised import FalsePositives
+from .supervised import TrueNegatives
+from .supervised import FalseNegatives
+from .supervised import Precision
+from .supervised import Recall
+from .supervised import FScore
