bd6b4fb602d9a6f7bfd14bb0bc6bb29aaff6ffd0
Chris Barrick <cbarrick1@gmail.com>
2018-04-24 01:43:44 -0400

    Simplify toys.context

--- toys/context.py
+++ toys/context.py
@@ -1,35 +1,7 @@
 from contextlib import contextmanager
 
 
-class Context:
-    def __init__(self, stack=None):
-        if stack is None:
-            stack = current_context().stack
-        self.stack = stack
-
-    def __copy__(self):
-        return self.copy()
-
-    def copy(self):
-        stack = self.stack.copy()
-        return Context(stack)
-
-    def push(self, *args, **kwargs):
-        frame = self.stack[-1].copy()
-        frame = frame.update(*args, **kwargs)
-        self.stack.append(frame)
-        return self
-
-    def pop(self):
-        return self.stack.pop()
-
-    def get(self, key, default=None):
-        frame = self.stack[-1]
-        return frame.get(key, default)
-
-
-_INITIAL_CONTEXT = Context([])
-_CURRENT_CONTEXT = _INITIAL_CONTEXT
+_CURRENT_CONTEXT = {}
 
 
 def current_context():
@@ -42,7 +14,7 @@ def context(*args, **kwargs):
     global _CURRENT_CONTEXT
     old = current_context()
     new = old.copy()
-    new.push(*args, **kwargs)
+    new.update(*args, **kwargs)
     _CURRENT_CONTEXT = new
     yield new
     _CURRENT_CONTEXT = old
