03aaf33e209c6f911765c8307b3383581b80afab
Chris Barrick <cbarrick1@gmail.com>
2018-04-25 18:25:11 -0400

    Simplify DataLoader

--- toys/datasets/utils/dataloader.py
+++ toys/datasets/utils/dataloader.py
@@ -7,8 +7,6 @@ from toys.datasets.utils import Zip
 
 class DataLoader(TorchDataLoader):
     def __init__(self, *datasets, **kwargs):
-        device_count = torch.cuda.device_count()
-        device_ids = list(range(device_count))
-        kwargs.setdefault('pin_memory', len(device_ids) > 0)
+        kwargs.setdefault('pin_memory', torch.cuda.is_available())
         dataset = Zip(*datasets)
         super().__init__(dataset, **kwargs)
