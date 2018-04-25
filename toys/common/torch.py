5a16b2b8c7d7f73638f822fbec57347106b60289
Chris Barrick <cbarrick1@gmail.com>
2018-04-25 19:34:58 -0400

    Fix TorchModel

--- toys/common/torch.py
+++ toys/common/torch.py
@@ -1,4 +1,4 @@
-from typing import Any, Sequence, Union
+from typing import Tuple
 import logging
 
 import numpy as np
@@ -19,30 +19,37 @@ class TorchModel(Model):
     This wrapper extends `torch.Module` to accept scalars, numpy arrays, and
     torch tensors as input and to return numpy arrays as output.
 
-    A TorchModel is aware of the number of dimensions expected for each input.
-    If an input has fewer dimensions, it is extended with trival dimensions.
+    A `TorchModel` is aware of the number of dimensions expected for each
+    input. If an input has fewer dimensions, trivial axes are added.
+
+    A `TorchModel` is NOT a `torch.Module`. Gradients are never computed.
 
     Attributes:
         module (Module):
             The module being wrapped.
-        dims (Sequence[int or None]):
-            The number of dimensions expected of each input.
-            A value of `None` means any shape is allowed.
+        dims (Tuple[int or None] or None):
+            The number of dimensions required of each input. If present,
+            the number and order of dimensions must match the number and
+            order of inputs expected by the module. A value of ``None``
+            means any shape is allowed for the corresponding input. If not
+            present, the number and shape of inputs is unconstrained.
     '''
 
     def __init__(self, module, *dims):
-        '''Construct a TorchModel.
+        '''Construct a `TorchModel`.
 
         Arguments:
             module (Module):
                 The module being wrapped.
             dims (int or None):
-                The number of dimensions required of each input. The number
-                and order of dimensions must match those expected by the
-                module. A value of `None` means any shape is allowed.
+                The number of dimensions required of each input. If present,
+                the number and order of dimensions must match the number and
+                order of inputs expected by the module. A value of ``None``
+                means any shape is allowed for the corresponding input. If not
+                present, the number and shape of inputs is unconstrained.
         '''
         self.module = module
-        self.dims = dims
+        self.dims = dims or None
 
     def __getattr__(self name):
         '''Attribute access is delecated to the underlying module.
@@ -56,22 +63,27 @@ class TorchModel(Model):
             inputs = self._cast_inputs(*inputs)
             y = self.module(*inputs)
             y = y.numpy()
-        return y
+            return y
 
     def _cast_inputs(self, *inputs):
         '''Cast inputs to tensors of the expected dtype, device, and dimension.
         '''
-        assert len(inputs) == len(self.dims)
+        assert self.dims is None or len(inputs) == len(self.dims)
         dtype = self.module.dtype
         device = self.module.device
 
-        for x, n_dims in zip(inputs, self.dims):
-            if np.isscalar(x): x = np.array(x)
-            if isinstance(x, np.ndarray): x = torch.from_numpy(x)
+        for i, x in enumerate(inputs):
+            if np.isscalar(x):
+                x = np.array(x)
+
+            if isinstance(x, np.ndarray):
+                x = torch.from_numpy(x)
+
             x = x.to(device, dtype)
 
-            assert x.dim() <= n_dims
-            for _ in range(x.dim(), n_dims):
-                x.unsqueeze_(0)
+            if self.dims and self.dims[i]:
+                assert x.dim() <= n_dims
+                for _ in range(x.dim(), n_dims):
+                    x = x.unsqueeze_(0)
 
             yield x
