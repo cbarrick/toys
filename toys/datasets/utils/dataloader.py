78c1fa43612340f090783d72310af103710f5516
Chris Barrick <cbarrick1@gmail.com>
2018-05-09 14:52:05 -0400

    Fix the zip step in DataLoader

--- toys/datasets/utils/dataloader.py
+++ toys/datasets/utils/dataloader.py
@@ -7,5 +7,8 @@ from .utils import Zip
 class DataLoader(TorchDataLoader):
     def __init__(self, *datasets, **kwargs):
         kwargs.setdefault('pin_memory', torch.cuda.is_available())
-        dataset = Zip(*datasets)
+        if 1 < len(datasets):
+            dataset = Zip(*datasets)
+        else:
+            dataset = datasets[0]
         super().__init__(dataset, **kwargs)
