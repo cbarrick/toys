3645fd5d89b82a38935c2ffafc2726e7b844e5c1
Chris Barrick <cbarrick1@gmail.com>
2018-06-01 16:01:12 -0400

    Restrict access to toys.common to internal users

--- toys/__init__.py
+++ toys/__init__.py
@@ -1,4 +1,3 @@
-from . import common
 from . import datasets
 from . import metrics
 from . import model_selection
@@ -6,6 +5,12 @@ from . import parsers
 from . import supervised
 from . import typing
 
+# External users should import these directly from the `toys` package.
+# Internal users should import these from `toys.common` to avoid import cycles;
+# this only applies to imports. Internal users can still call, e.g. `toys.zip`.
+from .common import BaseEstimator
+from .common import TunedEstimator
+from .common import TorchModel
 from .common import batches
 from .common import flatten
 from .common import subset
